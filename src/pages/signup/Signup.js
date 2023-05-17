f46a135e5c56ce10392c380b6301fccfedb440b8
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-17 14:52:18 +0530

    Created navbar

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,6 +1,19 @@
+import "./sinup.css"
 export function Singup1()
 {
     return(<div>
-        
+        <header className="topSection">
+            <div className="topSectionBox">
+                <div>
+                    <p className="heading1">Wheels of <span style={{ color: "orangered" }}>Fortune</span></p>
+
+                </div>
+                <nav>
+                    <a href="/" className="linksStyling">Login</a>
+                    <a href="/" className="linksStyling">Cart</a>
+                    <a href="/" className="linksStyling">Whislist</a>
+                </nav>
+            </div>
+        </header>
     </div>)
 }
