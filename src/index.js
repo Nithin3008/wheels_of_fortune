55d6b0b3a9365375d01b24b0d7405c6934d5daf7
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-17 15:33:02 +0530

    Added router, category data

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CategoryProvider } from "./pages/DataProviders/CategoryProv";
+
 
 // Call make Server
 makeServer();
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <CategoryProvider>
+      <App />
+      </CategoryProvider>
+    
+    </Router>
+    
   </React.StrictMode>,
   document.getElementById("root")
 );
