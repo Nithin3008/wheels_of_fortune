b51dc81a5195b42436117876e8e6f51d109d88ba
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-17 10:21:48 +0530

    Added nav bar and content

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
-
+import { Link } from "react-router-dom"
+import "./home.css"
 export function Home1() {
     const [category, setCategory] = useState([])
     useEffect(() => {
@@ -7,9 +8,35 @@ export function Home1() {
             const data = await fetch("/api/categories")
             const recData = await data.json()
             setCategory(recData.categories)
+            console.log(recData.categories)
         }; url();
     }, [])
     return (<div>
+        <div className="mainBox">
+            <header className="topSection">
+                <div className="topSectionBox">
+                    <div>
+                        <p className="heading1">Wheels of <span style={{ color: "orangered" }}>Fortune</span></p>
 
-    </div>)
+                    </div>
+                    <nav>
+                        <a href="/" className="linksStyling">Login</a>
+                        <a href="/" className="linksStyling">Cart</a>
+                        <a href="/" className="linksStyling">Whislist</a>
+                    </nav>
+                </div>
+            </header>
+            <section className="contentContainer">
+                <div className="contentBox">
+                    <div>
+                        <p className="heading1 contentHeading">Welcome to Wheels of <span style={{ color: "orangered" }}>Fortune</span></p>
+                        <p className="heading1">You can your purchase car which suits you</p>
+                    </div>
+                    <div>
+                        <img className="images" src="https://github.com/Nithin3008/wheels_of_fortune/blob/main/public/Images/Category/suv.jpg?raw=true"></img>
+                    </div>
+                </div>
+            </section>
+        </div >
+    </div >)
 }
