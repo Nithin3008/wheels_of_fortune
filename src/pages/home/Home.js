b62cf46f153485fdfee004ce493e1bf3e6ea94fc
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-17 09:45:07 +0530

    Getting category data using api

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,15 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export function Home1() {
+    const [category, setCategory] = useState([])
     useEffect(() => {
-
+        const url = async () => {
+            const data = await fetch("/api/categories")
+            const recData = await data.json()
+            setCategory(recData.categories)
+        }; url();
     }, [])
+    return (<div>
+
+    </div>)
 }
