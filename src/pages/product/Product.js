4aec87e7c34da71bd1f1e0a30d816bf87f3376eb
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-17 18:49:40 +0530

    Added filters and products

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,8 +1,10 @@
 import { useContext } from "react"
 import { CategoryData } from "../DataProviders/CategoryProv"
+import { ProductData } from "../DataProviders/ProductsProvid";
 import "./product.css"
 export function Product1()
 {
+    const {producData}=useContext(ProductData)
     return (
       <div>
             <header className="topSection">
@@ -25,6 +27,58 @@ export function Product1()
                     </nav>
                 </div>
             </header>
+            <section>
+                <div className="productBox">
+                    <div className="producBoxFilters">
+                        <div style={{display:"flex"}}><p style={{marginRight:"50px"}}>Filters</p><p style={{color:"#185464"}}>Clear</p></div>
+                        <p>Price</p>
+                        <input className="range" type="range" min="0" max="4000000"></input>
+                        <div>
+                            <p>Category</p>
+                            <div className="categoryFilter">
+                            <label> <input type="checkbox"></input> Truck</label>
+                            <label> <input type="checkbox"></input> Suv</label>
+                            <label> <input type="checkbox"></input> EV</label>
+                            <label> <input type="checkbox"></input> Super Cars</label>
+                            </div>
+                            
+                        </div>
+                        <div>
+                            <p >Rating</p>
+                            <div className="categoryFilter">
+                            <label> <input type="radio"></input> 1 Star+</label>
+                            <label> <input type="radio"></input> 2 Star+</label>
+                            <label> <input type="radio"></input> 3 Star+</label>
+                            <label> <input type="radio"></input> 4 Star+</label>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Price Sort</p>
+                            <div className="categoryFilter">
+                            <label><input type="radio"></input> low to high</label>
+                            <label><input type="radio"></input> high to low</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="producBoxCards">
+                        {producData.map((val)=>
+                        <div className="productCards">
+
+                            <div className="productImages">
+                                <img src={`${val.src}`}></img>
+                                </div>
+                            <div>
+                                <p>Title: {val.title}</p>
+                                <p>Manufacturer: {val.manufacturer}</p>
+                                <p>Power: {val.HP}</p>
+                                <p><button>Add to Cart</button></p>
+                            </div>
+
+                            </div>)}
+                    </div>
+                </div>
+            </section>
+
       </div>
     );
 }
