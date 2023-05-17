b5628632d16c3f41279d8f0323061f9081e96c60
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-17 15:13:05 +0530

    added new tags

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -15,14 +15,16 @@ export function Signup1()
                 </nav>
             </div>
         </header>
-        <section>
+        <section className="contentBox1">
             <div className="signBox1">
             <p className="heading1">Login</p>
                 <input placeholder={"First Name"}></input>
                 <input placeholder={"Last Name"}></input>
                 <input placeholder={"User Name"}></input>
                 <input placeholder={"Password"}></input>
-                <button className="buttonSty">Login</button>
+                <input placeholder={"Confirm Password"}></input>
+                <button className="buttonSty">Register</button>
+                <p>Already have account <a href="/" style={{color:"orangered",textDecoration:"none"}}>Login!</a></p>
             </div>
         </section>
     </div>)
