b8f20473cbc347623d1e6fa5e6c38c0ba7a7a65e
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-18 12:45:50 +0530

    collecting signup details for registration

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,6 +1,20 @@
 import "./signup.css"
+import {Sg} from "../DataProviders/Api/SignupApi"
+import { useState } from "react"
 export function Signup1()
 {
+    const signupDetails=
+    {
+        firstname:"",
+        lastname:"",
+        email:"",
+        password:"",
+    }
+    const [signupData,setSignup]=useState({})
+    function settingData()
+    {
+        setSignup(signupDetails) 
+    }
     return(<div>
         <header className="topSection">
             <div className="topSectionBox">
@@ -18,14 +32,17 @@ export function Signup1()
         <section className="contentBox1">
             <div className="signBox1">
             <p className="heading1">Login</p>
-                <input placeholder={"First Name"}></input>
-                <input placeholder={"Last Name"}></input>
-                <input placeholder={"User Name"}></input>
-                <input placeholder={"Password"}></input>
-                <input placeholder={"Confirm Password"}></input>
-                <button className="buttonSty">Register</button>
+                <input type="text" onChange={(e)=>signupDetails.firstname=e.target.value} placeholder={"First Name"}></input>
+                <input type="text" onChange={(e)=>signupDetails.lastname=e.target.value} placeholder={"Last Name"}></input>
+                <input type="email" onChange={(e)=>signupDetails.email=e.target.value} placeholder={"E-mail"}></input>
+                <input type="password" onChange={(e)=>signupDetails.password=e.target.value} placeholder={"Password"}></input>
+                <input type="password" onChange={(e)=>signupDetails.password=e.target.value} placeholder={"Confirm Password"}></input>
+               
+                <button onClick={()=>settingData()} className="buttonSty">Register</button>
                 <p>Already have account <a href="/" style={{color:"orangered",textDecoration:"none"}}>Login!</a></p>
+                
             </div>
         </section>
+        <Sg details={signupData}></Sg>
     </div>)
 }
