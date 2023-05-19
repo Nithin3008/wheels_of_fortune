8d319a8f12a66e9dce7d96de792522026e05f6e0
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-19 15:57:09 +0530

    refactor: Changed button to Link

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,6 +2,7 @@
 import "./login.css"
 import { useContext } from "react"
 import { LoginContext } from "../DataProviders/LoginProv"
+import { Link } from "react-router-dom"
 
 export function Login1() {
     const { userDet, authToken, loginHandler } = useContext(LoginContext)
@@ -14,9 +15,8 @@ export function Login1() {
 
                 </div>
                 <nav>
-                    <a href="/" className="linksStyling">Login</a>
-                    <a href="/" className="linksStyling">Cart</a>
-                    <a href="/" className="linksStyling">Whislist</a>
+                    <Link href="/Cart1" className="linksStyling">Cart</Link>
+                    <Link href="/Whislist1" className="linksStyling">Whislist</Link>
                 </nav>
             </div>
         </header>
@@ -26,7 +26,7 @@ export function Login1() {
                 <input onChange={(e)=>userDet.email=e.target.value} type="email" placeholder={"User Name"}></input>
                 <input onChange={(e)=>userDet.password=e.target.value} type="password" placeholder={"Password"}></input>
                 <button onClick={()=>loginHandler()} className="buttonSty">Login</button>
-                <p>Didn't have account <span style={{color:"orangered",fontSize:"20px"}}>Sign up!</span></p>
+                <p>Didn't have account <Link to="/Signup1" style={{color:"orangered",fontSize:"20px"}}>Sign up!</Link></p>
             </div>
             
         </section>
