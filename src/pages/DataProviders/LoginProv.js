4933f7fecb0b837888055f53df2b5efff4e260a2
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-20 13:55:21 +0530

    feat: Added logout handler, navigation

--- src/pages/DataProviders/LoginProv.js
+++ src/pages/DataProviders/LoginProv.js
@@ -1,30 +1,42 @@
 import axios from "axios";
 import { createContext, useState} from "react";
+import { useNavigate } from "react-router-dom";
 export const LoginContext=createContext()
+
 export function LoginProvider( { children }) {
+  const nav=useNavigate()
   let userDet={
     email: "",
     password: ""
   }
   const [authToken,setToken]=useState("empty")
+  
   const loginHandler = async () => {
     try {
+      console.log(userDet)
       const url = await axios.post("/api/auth/login", {
         email: userDet?.email,
         password: userDet?.password,
       });
-      setToken(url.data.encodedToken);
-      console.log(authToken)
-    } catch (error) {
+      if(url.status===200)
+      {
+        setToken(url.data.encodedToken)
+        nav("/")
+      }    
+      } catch (error) {
       console.log(error);
     }}
- 
+    function logutHandler()
+    {
+      localStorage.clear("token")
+      nav("/")
+    }
   
 
   return (
     <LoginContext.Provider value={{
       userDet, authToken, loginHandler
-      , setToken
+      , setToken,logutHandler
     }}>
       {children}
     </LoginContext.Provider>
