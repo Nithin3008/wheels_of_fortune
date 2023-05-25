cbaed8a20afc5befacb10fcce6a816b9cba8cb03
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-25 10:30:29 +0530

    Added new user

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -16,4 +16,13 @@ export const users = [
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
+  {
+    _id: uuid(),
+    firstName: "john",
+    lastName: "wick",
+    email: "test@gmail.com",
+    password: "test",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
 ];
