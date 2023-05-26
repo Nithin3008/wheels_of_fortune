00c0b07198c4eec868e01287a755f94839f996db
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-26 11:03:13 +0530

    changes in address property
    Now address can store multiple addresses

--- src/pages/DataProviders/MainReducer.js
+++ src/pages/DataProviders/MainReducer.js
@@ -1,133 +1,71 @@
-import { createContext, useReducer,useContext } from "react";
+import { createContext, useReducer, useContext } from "react";
 import axios from "axios";
 
+export const MainContext = createContext();
 
-export const MainContext=createContext()
+export function MainProvider({ children }) {
+    const MainData = {
+        isLoggedin: false,
+        user: {
+            fName: "",
+            lName: "",
+            userName: "",
+        },
+        address:[ {
+            street: "Wall street lower manhattam",
+            code: 10005,
+            city: "New York",
+            country: "USA",
+        },],
 
-export function MainProvider({children})
-{
-  
-    const MainData={
-        isLoggedin:false,
-        user:{
-            fName:"",
-            lName:"",
-            userName:""
-        },
-        address:{
-            street:"Wall street lower manhattam",
-            code:10005,
-            city:"New York",
-            country:"USA"
-        },
-    
-        products:[],
-        Cart:[],
-        WhisList:[]
+        products: [],
+        Cart: [],
+        Whislist: [],
+    };
+
+    const [state, dispatcherMain] = useReducer(MainFun, MainData);
+    function MainFun(state, action) {
+        if (action.type == "AddingProd") {
+            return { ...state, products: action.payload };
+        } 
+        else if (action.type == "LoginHandler") {
+            console.log(action.payload)
+                const x={
+                    fName: action.payload.fName,
+                    lName: action.payload.lName,
+                    userName: action.payload.userName,
+                }
+            return { ...state, isLoggedin: !state.isLoggedin,user:x };}
+       else if (action.type == "TestDetails") {
+        } else if (action.type == "getCart") {
+            return {...state,Cart:action.payload}
+        } 
+        else if (action.type == "AddCartItem") {
+            console.log(action.payload)
+            return {...state,Cart:action.payload}
+    }
+    else if(action.type=="AddWhislistItem")
+    {   
+        
+        return {...state, Whislist:action.payload}
     }
     
-    const [state,dispatcherMain]=useReducer(MainFun,MainData)
-    function MainFun(state,action)
-    {
-        
-        
-        if(action.type=="AddingProd")
-        {
-            
-             return {...state,products:action.payload}
-        }
-       else if(action.type=="LoginHandle")
-        {
-            return{...state,isLoggedin:!state.isLoggedin}
-        }
-        else if(action.type=="userDetails")
-        {
-            const x={
-                fname:action.payload.fName,
-                lname:action.payload.lName,
-                userName:action.payload.userName
-            }
-            return{...state,user:x}
-        }
-        else if(action.type=="TestDetails")
-        {
 
-        }
-        else if(action.type="getCart")
-        {
-            const encodedToken=localStorage.getItem("token")
-            const getCart = async () => {
-                try {
-                    const response = await axios.get(`/api/user/cart`, {
-                      headers: {
-                        authorization: encodedToken, // passing token as an authorization header
-                      },
-                    });
-                    if(response.status==200)
-                    {
-                        return {...state,Cart:response.data.cart}
-                    }
-                    
-                  } catch (error) {
-                    console.log(error);
-                  }
-              };
-             getCart()
-        }
-        // else if(action.type=="AddToCart")
-        // {
-           
-        // }
-        
-               
-         
-     
-        // {
-        //     case "LoginSucess":
-                
-        //             return{...state,isLoggedin:!state.isLoggedin};
-                
-       
-                
-                
-        //     // case "AddingCate":
-        //     //     console.log("prd loadong")
-        //     //     return {...state,category:action.payload}
-             
-        //     case "AddingProd":
-        //         console.log("prd loadong")
-        //         return {...state,products:action.payload}            
+    return state;
+}
 
-        //     //     
-        //     // case "AddToWhislist":
-        //     //     {   
-        //     //         const x=Cart.find((val)=>val.id==action.payload)
-        //     //         return {...state,WhisList:[...state.WhisList,x]}
-
-        //     //     }
-        //     // case "RemoveFromCart":
-        //     //     {
-        //     //         const x=WhisList.filter((val)=>val.id!==action.payload)
-        //     //         return {...state,Cart:[...state.Cart,...x]}
-
-        //     //     }    
-        //     // case "RemoveFromWhisList":
-        //     //     {
-        //     //         const x=WhisList.filter((val)=>val.id!==action.payload)
-        //     //         console.log("hi")
-        //     //         return {...state,WhisList:[...state.WhisList,...x]}
-    
-        //     //     }  
-        //     default:
-        //         return state  
-        return state
-        }
-        console.log("cart loading",state.Cart)
-    return(<MainContext.Provider value={{dispatcherMain,ProdDetails:state.products,LoginId:state.isLoggedin,CartData:state.Cart,WhisListData:state.WhisList}}>
+console.log("cart loading",state.user)
+return (
+    <MainContext.Provider
+        value={{
+            dispatcherMain,
+            ProdDetails: state.products,
+            LoginId: state.isLoggedin,
+            CartData: state.Cart,
+            WhisListData: state.Whislist,
+        }}
+    >
         {children}
-    </MainContext.Provider>)
+    </MainContext.Provider>
+);
     }
-
-
-
-
