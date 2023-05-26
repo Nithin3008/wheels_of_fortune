61b2e3a763b41624c2efbbf1c2c540bda294f816
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-26 11:00:38 +0530

    Added new functionality
    Add to whilst, remove cart item

--- src/pages/DataProviders/FuncCall.js
+++ src/pages/DataProviders/FuncCall.js
@@ -1,16 +1,18 @@
 import { createContext,useContext } from "react";
 import { MainContext } from "../DataProviders/MainReducer";
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export const FuncContext=createContext()
 export function FuncProvider({ children }) {
 
-    const {ProdDetails,LoginId,dispatcherMain}=useContext(MainContext)
+    const {CartData,ProdDetails,LoginId,dispatcherMain}=useContext(MainContext)
 
 
-  const getCartData = async (id) => {
+  const pushCartData = async (id) => {
     const product = ProdDetails.find((val) => val._id == id);
 
-    console.log("repeating 2 times");
+    
     const encodedToken = localStorage.getItem("token");
     const getCart = async () => {
       try {
@@ -27,7 +29,8 @@ export function FuncProvider({ children }) {
         );
         console.log(response.data.cart);
         if (response.status === 201) {
-          
+          toast.success("Item added to Cart",{
+            position:"bottom-right"})
           dispatcherMain({ type: "AddCartItem", payload: response.data.cart });
         }
       } catch (error) {
@@ -50,7 +53,7 @@ const cartFetch=async()=>
             // console.log("cart loading",response.data)
             // 
             if (response.status == 200) {
-               dispatcherMain({type:"addToCart",payload:response.data.cart})
+               dispatcherMain({type:"getCart",payload:response.data.cart})
                 
             }
         } catch (error) {
@@ -58,6 +61,66 @@ const cartFetch=async()=>
         }
     };
     getCart();
+}
+
+
+const pushWhislistData = async (id) => {
+    const product = CartData.find((val) => val._id == id);
+
+    console.log("hi whist I'm ur cart",CartData,product);
+    const encodedToken = localStorage.getItem("token");
+    const getWhislist = async () => {
+      try {
+        const response = await axios.post(
+          "/api/user/wishlist",
+          {
+            product,
+          },
+          {
+            headers: {
+              authorization: encodedToken,
+            },
+          }
+        );
+        console.log(response.data);
+        if (response.status === 201) {
+          toast.success("Added to Whislist",{
+            position:"bottom-right"})
+          dispatcherMain({ type: "AddWhislistItem", payload: response.data.wishlist });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getWhislist();
+  };
+const removeCartItem= async(id)=>
+{
+   const removeItem=async()=>
+   {
+    const encodedToken = localStorage.getItem("token");
+    try {
+        const response=await axios.delete(`/api/user/cart/${id}`,
+         {   
+        headers: {
+              authorization: encodedToken,
+            },
+        }
+       
+        
+        )
+        if(response.status==200)
+       {
+        toast.warning("Removed from Cart",{
+          position:"bottom-right"})
+        dispatcherMain({type:"AddCartItem",payload:response.data.cart})
+       }
+        console.log(response.data)
+    } catch (error) {
+        console.log("error")
+    }
+   }
+   removeItem()
 }
 
 
@@ -71,13 +134,10 @@ const cartFetch=async()=>
 
 
 
-
-
-
-
   return (<>
-  <FuncContext.Provider value={{getCartData}}>
+  <FuncContext.Provider value={{pushCartData,cartFetch,pushWhislistData,removeCartItem}}>
     {children}
   </FuncContext.Provider>
+  <ToastContainer />
   </>);
 }
