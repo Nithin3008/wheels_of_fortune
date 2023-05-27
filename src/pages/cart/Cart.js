49df9afb895f1626b0a5377ad1800cd0b146c192
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-27 11:38:31 +0530

    Added Quantity, checkout features

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,22 +1,31 @@
-import { useContext, useEffect } from "react";
+import { useContext} from "react";
 import { useNavigate } from "react-router-dom";
 import "./cart.css"
 import { MainContext } from "../DataProviders/MainReducer";
+import { FuncContext } from "../DataProviders/FuncCall";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export function Cart1()
 {
-  const {CartData,LoginId,dispatcherMain}=useContext(MainContext)
- 
+  const {CartData,LoginId}=useContext(MainContext)
+  const {pushWhislistData,removeCartItem,increItem,decreItem}=useContext(FuncContext)
     
     const nav=useNavigate()
-    function removeCart(id)
-    {
-        dispatcherMain({type:"RemoveFromCart",payload:id})
-    }
+   
     function addtoWhislist(id)
     {
-        dispatcherMain({type:"AddToWhislist",payload:id})
+      pushWhislistData(id)
+      
+      console.log("now adding int whislist")
+      removeCartItem(id)
+      
     }
-    console.log(CartData)
+    function deleteItem(id)
+    {
+      removeCartItem(id)
+    }
+   
+    console.log("hello cart")
     return(<div>
        <header className="topSection">
           <div className="topSectionBox">
@@ -41,18 +50,20 @@ export function Cart1()
         <div className="CartBox">
             <div className="cartItems">
                 {CartData.map((val)=>
-                        <div key={val.id} className="productCards">
+                        <div key={val.title} className="productCards">
 
-                            <div key={val.id} className="productImages">
+                            <div  className="productImages">
                                 <img src={`${val.src}`}></img>
                                 </div>
-                            <div>
+                            <div key={val._id}>
                                 <p>Title: {val.title}</p>
                                 <p>Manufacturer: {val.manufacturer}</p>
                                 <p>Power: {val.HP}</p>
                                 <p>Price: {val.price}$</p>
-                                <button onClick={()=>removeCart(val._id)} className="buttonSty cart">Remove from Cart</button>
-                                <button onClick={()=>addtoWhislist(val._id)} className="buttonSty cart">Move to WhisList</button>
+                                <p>Quantity:<button disabled={val.qty==0} onClick={()=>decreItem(val
+                                  ._id)}>-</button> {val.qty}<button onClick={()=>increItem(val._id)}>+</button></p>
+                                <button onClick={()=>deleteItem(val._id)} className="buttonSty cart">Remove from Cart</button>
+                                <button  onClick={()=>addtoWhislist(val._id)} className="buttonSty cart">Move to WhisList</button>
                             </div>
 
                             </div>)}
@@ -62,10 +73,10 @@ export function Cart1()
                     <div>
                         <h2>Order Details</h2>
                         <hr></hr>
-                       <div> {CartData.map(({title,price})=><p>{title}-  {price}$</p>)}</div>
+                       <div > {CartData.map(({title,price})=><p>{title}-  {price}$</p>)}</div>
                         <hr></hr>
-                       <div>Total Price: {CartData.reduce((acc,{price})=>(price+acc),0)}$</div>
-                       <button className="buttonSty">Proceed with checkout</button>
+                       <div >Total Price: {CartData.reduce((acc,{price})=>(price+acc),0)}$</div>
+                       <button onClick={()=>nav("/Checkout1")} className="buttonSty">Proceed with checkout</button>
                     </div>
             </div>
            
