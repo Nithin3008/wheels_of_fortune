55507f55075cbbfa69441547dfd45729a07ee301
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-28 18:28:34 +0530

    Create Profile.js

---
+++ src/pages/userProfile/Profile.js
@@ -0,0 +1,23 @@
+import { useContext } from "react"
+import { MainContext } from "../DataProviders/MainReducer";
+import { FuncContext } from "../DataProviders/FuncCall";
+export function Profile1()
+{
+    const {ProfileDetails}=useContext(MainContext)
+    const {logoutUser}=useContext(FuncContext)
+    
+    return(<div>
+        <header>
+            THis is profile
+        </header>
+            <div>
+                <h1>My Account</h1>
+                <div>
+                   <p>Name: <span>{ProfileDetails.fname}</span></p>
+                   <p>Username: <span>{ProfileDetails.userName}</span></p>
+                   <button onClick={()=>logoutUser()}>Log out</button>
+                </div>
+                
+            </div>
+    </div>)
+}
