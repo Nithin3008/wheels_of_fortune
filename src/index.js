60ec9302b63eee7b1cff2db089d57286413d15c2
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-28 18:28:57 +0530

    Reducing contexts

--- src/index.js
+++ src/index.js
@@ -4,32 +4,23 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
-import { CategoryProvider } from "./pages/DataProviders/CategoryProv";
+// import { CategoryProvider } from "./pages/DataProviders/CategoryProdProv";
 
 import { AuthProvider } from "./pages/DataProviders/AuthProvider";
 import { MainProvider } from "./pages/DataProviders/MainReducer";
+import { FuncProvider } from "./pages/DataProviders/FuncCall";
 // Call make Server
 makeServer();
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-    <MainProvider> 
-    <AuthProvider>
-    
-      
-      <CategoryProvider>
-
-      <App />
-      </CategoryProvider>
-
-    
-     
-      </AuthProvider>
-      </MainProvider>
-    </Router>
-    
-  </React.StrictMode>,
+  <Router>
+    <MainProvider>
+      <FuncProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </FuncProvider>
+    </MainProvider>
+  </Router>,
   document.getElementById("root")
 );
