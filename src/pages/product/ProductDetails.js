21a3393d4c7e4b8dcfed27ef9956203f3f461f92
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-29 14:40:40 +0530

    shows details about the prod
    Now we can add the item to cart and Wishlist from prodDetails page

--- src/pages/product/ProductDetails.js
+++ src/pages/product/ProductDetails.js
@@ -1,18 +1,32 @@
 import { useContext } from "react";
 import { MainContext } from "../DataProviders/MainReducer";
 import { useParams } from "react-router-dom";
+import { FuncContext } from "../DataProviders/FuncCall";
+import "./prdDetails.css"
 export function ProductDetails1()
 {
     const {ProdDetails}=useContext(MainContext)
-    const itemId=useParams()
-    const item=ProdDetails.find((val)=>val._id===itemId)
+    const {pushCartData,pushWhislistData,itemInCart}=useContext(FuncContext)
+    const {prodId}=useParams()
+    const item=ProdDetails.find((val)=>val._id===prodId)
+    console.log(item._id)
+    console.log(prodId)
     return(<div>
         <header>
             <h1>Product Details page</h1>
         </header>
-        <section>
-            <div>
-
+        <section className="prodDetails">
+            <div className="prodImage">
+                <img src={item.src}></img>
+            </div>
+            <div className="prodDet">
+                <h2>{item.title}</h2>
+                <p>Manufacturer: {item.manufacturer}</p>
+                <p>HP:{item.HP}</p>
+                <p>Torque: {item.Torque}</p>
+                <p>Rating: {item. rating}</p>
+                <button disabled={itemInCart(item._id)==item._id} onClick={()=>pushCartData(item._id)}>{itemInCart(item._id)===item._id?"Go to Cart":"Add to cart"}</button>
+                <button onClick={()=>pushWhislistData(item._id)}>Move to Wishlist</button>
             </div>
         </section>
     </div>)
