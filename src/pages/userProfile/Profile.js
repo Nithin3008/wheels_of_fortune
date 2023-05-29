7ca763004238111218a76be494b57378c4dd5086
Nithin <60174062+Nithin3008@users.noreply.github.com>
2023-05-29 14:49:32 +0530

    Added first and last name

--- src/pages/userProfile/Profile.js
+++ src/pages/userProfile/Profile.js
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { MainContext } from "../DataProviders/MainReducer";
 import { FuncContext } from "../DataProviders/FuncCall";
+import "./profile.css"
 export function Profile1()
 {
     const {ProfileDetails}=useContext(MainContext)
@@ -9,10 +10,11 @@ export function Profile1()
         <header>
             THis is profile
         </header>
-            <div>
+            <div className="userDetails">
                 <h1>My Account</h1>
                 <div>
-                   <p>Name: <span>{ProfileDetails.fName}</span></p>
+                   <p>First Name: <span>{ProfileDetails.fName}</span></p>
+                   <p>Last Name: <span>{ProfileDetails.lName}</span></p>
                    <p>Username: <span>{ProfileDetails.userName}</span></p>
                    <button onClick={()=>logoutUser()}>Log out</button>
                 </div>
