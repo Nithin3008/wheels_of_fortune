7f947c336c4c252e292b3bc6c87f72acf892e318
Nithin <nithindevathi@gmail.com>
2023-06-01 11:57:56 +0530

    Added authentication checking

--- src/components/navBar/nav.js
+++ src/components/navBar/nav.js
@@ -4,17 +4,27 @@ import { MainContext } from "../../pages/DataProviders/MainReducer"
 import "./nav.css"
 export function NavBar()
 {
-    const {LoginId}=useContext(MainContext)
+
+    const {LoginId,dispatcherMain}=useContext(MainContext)
     const nav=useNavigate()
+    const searchBox=(event)=>{
+        const item=(event.target.value).toLowerCase()
+        dispatcherMain({type:"searchQuery",payload:item})
+        nav("/Product1")
+      }
+    
+    
     return(<div>
         <div className="NavBox">
             <div className="Logo"><p>Wheels of <span>Fortune</span></p></div>
-            <div className="searchBox"><input type="search" placeholder="Search"></input></div>
+            <div className="searchBox"><input onChange={(e)=>searchBox(e)} type="search" placeholder="Search"></input></div>
             <div className="NavLinks">
-            <button onClick={()=>LoginId?"":nav("/Login1")}>Login</button>
+            <button onClick={()=>LoginId?nav("/Login1"):nav("/Login1")}>Login</button>
             <button onClick={()=>nav("/Product1")}>Explore</button>
             <button onClick={()=>LoginId?nav("/Cart1"):nav("/Login1")}>Cart</button>
-            <button onClick={()=>LoginId?nav("/Whislist1"):nav("/Login1")}>WishList</button></div>
+            <button onClick={()=>LoginId?nav("/Whislist1"):nav("/Login1")}>WishList</button>
+            <button onClick={()=>LoginId?nav("/Profile1"):nav("/Login1")}>Profile</button>
+            </div>
         </div>
 
 
