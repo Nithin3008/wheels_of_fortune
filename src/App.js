ec0805cfb58f8acb3a8a0cb284feb5abc994fef9
Nithin <nithindevathi@gmail.com>
2023-06-01 17:10:21 +0530

    reformatted code

--- src/App.js
+++ src/App.js
@@ -2,27 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home1 } from "./pages/home/Home";
 import { Login1 } from "./pages/Login/Login";
-import{Signup1} from "./pages/signup/Signup"
+import { Signup1 } from "./pages/signup/Signup"
 import { Product1 } from "./pages/product/Product";
-// import {ProductProvider} from "./pages/DataProviders/ProductsProvid"
 import { Cart1 } from "./pages/cart/Cart";
 import { Whislist1 } from "./pages/whislist/WhisList";
 import { Checkout1 } from "./pages/checkout/checkout";
 import { Clear } from "./clearToken";
-import {Profile1} from "./pages/userProfile/Profile"
-import {ProductDetails1} from "./pages/product/ProductDetails"
-import { Loader } from "./components/spinner";
+import { Profile1 } from "./pages/userProfile/Profile"
+import { ProductDetails1 } from "./pages/product/ProductDetails"
+
 function App() {
   return (
     <div className="App">
-   
-      
-      {/* <Home1></Home1> */}
-      {/* <Login1></Login1> */}
-      {/* <Signup1></Signup1> */}
       <Clear></Clear>
       <Routes>
-      <Route path="" element={<Home1 />}></Route>
+        <Route path="" element={<Home1 />}></Route>
         <Route path="/Login1" element={<Login1 />}></Route>
         <Route path="/Signup1" element={<Signup1 />}></Route>
         <Route path="/Cart1" element={<Cart1 />}></Route>
@@ -32,7 +26,7 @@ function App() {
         <Route path="/ProductDetails1/:prodId" element={<ProductDetails1 />}></Route>
         <Route path="/Profile1" element={<Profile1 />}></Route>
       </Routes>
-  
+
 
 
 
