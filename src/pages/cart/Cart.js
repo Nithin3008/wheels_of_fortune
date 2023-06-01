ae321b54d0e139370aa903b3018e014736dbed23
Nithin <nithindevathi@gmail.com>
2023-06-01 17:07:14 +0530

    minor fixes

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -3,12 +3,11 @@ import { useNavigate } from "react-router-dom";
 import "./cart.css"
 import { MainContext } from "../DataProviders/MainReducer";
 import { FuncContext } from "../DataProviders/FuncCall";
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { NavBar } from "../../components/navBar/nav";
 export function Cart1()
 {
-  const {CartData,LoginId}=useContext(MainContext)
+  const {CartData}=useContext(MainContext)
   const {pushWhislistData,removeCartItem,increItem,decreItem}=useContext(FuncContext)
   const totalPrice=CartData.reduce((acc,{qty,price})=>(acc+qty*price),0)
     const nav=useNavigate()
@@ -30,19 +29,19 @@ export function Cart1()
         {CartData.length<=0?<h1 style={{textAlign:"center",padding:"10px 10px",margin:"20px 20px"}}>Cart is Empty</h1>:""}
         <div className="CartBox">
          
-            <div className="cartItems">
+            <div className="carItems">
                 {CartData.map((val)=>
                         <div key={val.title} className="productCards">
 
                             <div  className="productImages">
-                                <img src={`${val.src}`}></img>
+                                <img src={`${val.src}`}alt="carImages"></img>
                                 </div>
                             <div key={val._id}>
                                 <p>Title: {val.title}</p>
                                 <p>Manufacturer: {val.manufacturer}</p>
                                 <p>Power: {val.HP}</p>
                                 <p>Price: {val.price}$</p>
-                                <p>Quantity:<button className="qtyBtn" disabled={val.qty==0} onClick={()=>decreItem(val
+                                <p>Quantity:<button className="qtyBtn" disabled={val.qty===0} onClick={()=>decreItem(val
                                   ._id)}>-</button> {val.qty}<button className="qtyBtn"  onClick={()=>increItem(val._id)}>+</button></p>
                                 <button onClick={()=>deleteItem(val._id)} className="cartBtn">Remove from Cart</button>
                                 <button  onClick={()=>addtoWhislist(val._id)} className="cartBtn">Move to WhisList</button>
