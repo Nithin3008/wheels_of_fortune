5619cb51496901a6065391dd545472da9893a839
Nithin <nithindevathi@gmail.com>
2023-06-01 17:05:43 +0530

    Removed unwanted context

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { CategoryProvider } from "./pages/DataProviders/CategoryProdProv";
-
-import { AuthProvider } from "./pages/DataProviders/AuthProvider";
 import { MainProvider } from "./pages/DataProviders/MainReducer";
 import { FuncProvider } from "./pages/DataProviders/FuncCall";
 // Call make Server
@@ -16,9 +13,9 @@ ReactDOM.render(
   <Router>
     <MainProvider>
       <FuncProvider>
-        <AuthProvider>
+
           <App />
-        </AuthProvider>
+
       </FuncProvider>
     </MainProvider>
   </Router>,
