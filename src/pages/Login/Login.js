3395c0aa17272929a0c62827d427cdcb8a293424
Nithin <nithindevathi@gmail.com>
2023-06-03 15:38:05 +0530

    Minor Fix

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -44,7 +44,7 @@ export function Login1() {
                 <p className="heading1">Login</p>
                 <input id="usn" onChange={(e)=>details.userName=e.target.value}   type="email" placeholder={"User Name"}></input>
                 <input id="pwd" onChange={(e)=>details.pwd=e.target.value}   type="password" placeholder={"Password"}></input>
-                <button onClick={()=>sendData2()}  className="buttonSty">Login with Test Creditanls</button>
+                <button onClick={()=>sendData2()}  className="buttonSty">Login with Test Credentials</button>
                 <button onClick={()=>sendData()}  className="buttonSty">Login</button>
                 <p>Didn't have account <Link to="/Signup1" style={{color:"orangered",fontSize:"20px",textDecoration:"none"}}>Sign up!</Link></p>
             </div>
