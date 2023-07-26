ce2cfd8ea55e6029b4284a3824ce0cf7c325f2fe
Nithin <nithindevathi@gmail.com>
2023-07-26 10:43:59 +0530

    minor changes

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -6,26 +6,15 @@ import { MainContext } from "../DataProviders/MainReducer";
 import { Loader } from "../../components/spinner";
 import "./product.css";
 import { NavBar } from "../../components/navBar/nav";
+import { FiltersPage } from "../../components/filters/FiltersPage";
 export function Product1() {
   const nav = useNavigate();
-  const {
-    LoginId,
-    ProdDetailsCate,
-    dispatcherMain,
-    ratingFilter,
-    Range,
-    SortBy,
-  } = useContext(MainContext);
+  const { LoginId, ratingFilter } = useContext(MainContext);
   const {
     pushCartData,
     itemInCart,
     itemInWishList,
-    shopCate,
     pushWhislistData,
-    removeCate,
-    setStars,
-    setRange,
-    clearAllFilter,
     removeWhisListItem,
     carsData,
   } = useContext(FuncContext);
@@ -37,29 +26,9 @@ export function Product1() {
   if (Range < 4000000) {
     prodData = prodData.filter((val) => val.price < Range);
   }
-  console.log("product page rendering");
   function AddToCart(event, id) {
     event.target.innerText = "Go to Cart";
     LoginId ? pushCartData(id) : nav("/Login1");
-  }
-  function setRating(event) {
-    console.log(event.target.value);
-    const stars = event.target.value;
-    setStars(stars);
-  }
-
-  function setCategory(event, cate) {
-    const str = event.target.checked;
-
-    str ? shopCate(cate) : removeCate(cate);
-  }
-  function priceRange(event) {
-    console.log(event.target.value);
-    const num = event.target.value;
-    setRange(num);
-  }
-  function cleanAll() {
-    clearAllFilter();
   }
   return (
     <div>
@@ -67,134 +36,10 @@ export function Product1() {
       <NavBar></NavBar>
       <section>
         <div className="productBox">
-          <div className="producBoxFilters">
-            <div style={{ display: "flex" }}>
-              <p style={{ marginRight: "50px" }}>Filters</p>
-              <p onClick={() => cleanAll()} style={{ color: "#185464" }}>
-                Clear
-              </p>
-            </div>
-            <p>Price</p>
-            <input
-              onChange={(e) => priceRange(e)}
-              defaultValue={3800000}
-              className="range"
-              type="range"
-              min="0"
-              max="3800001"
-            ></input>
-            <div>
-              <p>Category</p>
-              <div className="categoryFilter">
-                <label>
-                  {" "}
-                  <input
-                    checked={ProdDetailsCate.includes("Trucks")}
-                    onClick={(e) => setCategory(e, "Trucks")}
-                    type="checkbox"
-                  ></input>{" "}
-                  Truck
-                </label>
-                <label>
-                  {" "}
-                  <input
-                    checked={ProdDetailsCate.includes("Suv")}
-                    onClick={(e) => setCategory(e, "Suv")}
-                    type="checkbox"
-                  ></input>{" "}
-                  Suv
-                </label>
-                <label>
-                  {" "}
-                  <input
-                    checked={ProdDetailsCate.includes("EV")}
-                    onClick={(e) => setCategory(e, "EV")}
-                    type="checkbox"
-                  ></input>{" "}
-                  EV
-                </label>
-                <label>
-                  {" "}
-                  <input
-                    checked={ProdDetailsCate.includes("Super Cars")}
-                    onClick={(e) => setCategory(e, "Super Cars")}
-                    type="checkbox"
-                  ></input>{" "}
-                  Super Cars
-                </label>
-              </div>
-            </div>
-            <div>
-              <p>Rating</p>
-              <div className="categoryFilter">
-                <label>
-                  {" "}
-                  <input
-                    checked={ratingFilter == 1 ? true : false}
-                    value={1}
-                    onClick={(e) => setRating(e)}
-                    type="radio"
-                  ></input>{" "}
-                  1 Star+
-                </label>
-                <label>
-                  {" "}
-                  <input
-                    checked={ratingFilter == 2 ? true : false}
-                    value={2}
-                    onClick={(e) => setRating(e)}
-                    type="radio"
-                  ></input>{" "}
-                  2 Star+
-                </label>
-                <label>
-                  {" "}
-                  <input
-                    checked={ratingFilter == 3 ? true : false}
-                    value={3}
-                    onClick={(e) => setRating(e)}
-                    type="radio"
-                  ></input>{" "}
-                  3 Star+
-                </label>
-                <label>
-                  {" "}
-                  <input
-                    checked={ratingFilter == 4 ? true : false}
-                    value={4}
-                    onClick={(e) => setRating(e)}
-                    type="radio"
-                  ></input>{" "}
-                  4 Star+
-                </label>
-              </div>
-            </div>
-            <div>
-              <p>Price Sort</p>
-              <div className="categoryFilter">
-                <label>
-                  <input
-                    checked={SortBy == "low2high" ? true : false}
-                    onClick={() =>
-                      dispatcherMain({ type: "lowToHigh", payload: "low2high" })
-                    }
-                    type="radio"
-                  ></input>{" "}
-                  low to high
-                </label>
-                <label>
-                  <input
-                    checked={SortBy == "high2low" ? true : false}
-                    onClick={() =>
-                      dispatcherMain({ type: "highToLow", payload: "high2low" })
-                    }
-                    type="radio"
-                  ></input>{" "}
-                  high to low
-                </label>
-              </div>
-            </div>
+          <div className="filterBox">
+            <FiltersPage></FiltersPage>
           </div>
+
           <div className="producBoxCards">
             {prodData.map((val) => (
               <div key={val._id} className="prdCards">
